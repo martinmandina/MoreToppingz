16087d3350781194e5febe16f7b7dc1b241c7d64
martinsmandina <martinsmandina@gmail.com>
2020-05-17 13:04:39 +0300

    addfontlink/alias function

---
+++ js/sila.js
@@ -0,0 +1,10 @@
+var myID = function( id ) { return document.getElementById( id ); };
+
+/*myID( 'someID' )
+Here I used $, but you can use any valid variable name.
+var myVar = document.getElementById("toppingsDropDown").value;
+var whatEver = myID('toppingsDropDown').value();
+
+var byId = function( id ) { return document.getElementById( id ); };
+
+byId( 'someID' )*/
