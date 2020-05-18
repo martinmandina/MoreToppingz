ed49dc46eebc47a4aa47ca454a8130dbbcadafe7
martinsmandina <martinsmandina@gmail.com>
2020-05-18 14:56:20 +0300

    add pizza function and price to add toppings/size

--- js/sila.js
+++ js/sila.js
@@ -15,3 +15,43 @@ function showDropOffPoint() {
   document.getElementById("dropOffPoint").style.visibility = "visible";
   document.getElementById("dropOffPoint").value = "";
 }
+var pizzaPrice = 500;
+
+function Pizza(size,toppings,pizzaPrice) {
+  this.size = size;
+  this.toppings = toppings;
+  this.pizzaPrice = 0;
+}
+
+Pizza.prototype.price = function() {
+  if (this.size === "Small") {
+    this.pizzaPrice += 200;
+  }
+  else if (this.size === "Medium") {
+    this.pizzaPrice += 300;
+  }
+  else if (this.size === "Large") {
+    this.pizzaPrice += 400;
+  }
+  if (this.toppings.indexOf("Cheese") >= 0) {
+    this.pizzaPrice += 300;
+  }
+
+  if (this.toppings.indexOf("Cheese") >= 0 && this.toppings.indexOf("Peanut butter") >= 0) {
+    this.pizzaPrice += 1000;
+    console.log("hey");
+
+  }
+
+ if (this.toppings.indexOf("Vegetarian") >= 0) {
+    this.pizzaPrice += 200;
+  }
+  if (this.toppings.indexOf("Supreme") >= 0) {
+    this.pizzaPrice += 400;
+  }
+  if (this.toppings.indexOf("Pepperoni") >= 0) {
+    this.pizzaPrice += 300;
+  }
+
+return this.pizzaPrice;
+}
