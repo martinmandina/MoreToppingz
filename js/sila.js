0f1cbca6ecd4249787171a14e21d2adae5054907
martinsmandina <martinsmandina@gmail.com>
2020-05-18 16:13:13 +0300

    resized window

--- js/sila.js
+++ js/sila.js
@@ -75,3 +75,13 @@ $(document).ready(function(){
 
       });
     });
+    // tell the embed parent frame the height of the content
+    if (window.parent && window.parent.parent){
+      window.parent.parent.postMessage(["resultsFrame", {
+        height: document.body.getBoundingClientRect().height,
+        slug: ""
+      }], "*")
+    }
+
+    // always overwrite window.name, in case users try to set it manually
+    window.name = "result"
